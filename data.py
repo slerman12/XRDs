01afc1bff656fd7f142382aa5c72d8c6e2802396
samlerman <slerman@ur.rochester.edu>
2021-10-18 00:51:14 -0400

    data loading

--- data.py
+++ data.py
@@ -11,22 +11,53 @@ class XRDData(Dataset):
         for y, class_name in enumerate(sub_dirs):
             class_data = []
             files = glob.glob(class_name + "/*")
+            # todo its fast to just count the sizes... can count sizes and get item via just reading one at a time
             for file in tqdm(files, desc="Reading " + class_name):
                 with open(file) as f:
                     lines = f.readlines()[3:]
+                    lines = lines[:round(len(class_data) * train_test_split)] if train \
+                        else lines[round(len(class_data) * train_test_split):]
                     lines = [list(map(float, line.strip().split(", "))) for line in lines]
                     x = torch.FloatTensor(lines)
                     data_point = (x, y)
-                    class_data.append(data_point)
-            if train:
-                class_data = class_data[:round(len(class_data) * train_test_split)]
-            else:
-                class_data = class_data[round(len(class_data) * train_test_split):]
-
-            self.xrds = self.xrds + class_data
+                    self.xrds.append(data_point)
 
     def __len__(self):
         return len(self.xrds)
 
     def __getitem__(self, idx):
         return self.xrds[idx]
+
+
+class XRDDataFast(Dataset):
+    def __init__(self, root, train=True, train_test_split=0.9):
+        sub_dirs = glob.glob(root + "/*/")
+        self.xrds = {}
+        self.size = 0
+        for class_name in sub_dirs:
+            files = glob.glob(class_name + "/*")
+            for file in files:
+                with open(file) as f:
+                    lines = f.readlines()[3:]
+                    self.xrds[class_name] = lines, len(self.xrds[class_name])
+        self.train = train
+        self.train_test_split = train_test_split
+
+    def __len__(self):
+        size = 0
+        for class_name in self.xrds:
+            for lines in self.xrds:
+                total_size = len(lines)
+                train_size = round(total_size * self.train_test_split)
+                if self.train:
+                    size += train_size
+                else:
+                    size = total_size - train_size
+        return size
+
+    def __getitem__(self, idx):
+        for class_name in self.xrds:
+            for y, lines in enumerate(self.xrds):
+                lines = [list(map(float, line.strip().split(", "))) for line in lines]
+                x = torch.FloatTensor(lines)
+                return x, y
--- train.py
+++ train.py
@@ -57,17 +57,21 @@ if __name__ == '__main__':
     lr = 0.01
 
     saved = glob.glob("./*.pt")
-    if './train_loader.pt' in saved and './test_loader.pt' in saved:
+    train_test_split = 0.9
+    if './train_loader.pt' in saved:
         train_loader = torch.load('train_loader.pt')
-        test_loader = torch.load('test_loader.pt')
     else:
-        train_test_split = 0.9
         train_dataset = XRDData(root='xrd_data', train=True, train_test_split=train_test_split)
-        test_dataset = XRDData(root='xrd_data', train=False, train_test_split=train_test_split)
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
         torch.save(train_loader, 'train_loader.pt')
+
+    if './test_loader.pt' in saved:
+        test_loader = torch.load('test_loader.pt')
+    else:
+        test_dataset = XRDData(root='xrd_data', train=False, train_test_split=train_test_split)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
         torch.save(test_loader, 'test_loader.pt')
 
     optim = SGD(model.parameters(), lr=lr)
