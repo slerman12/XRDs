e3934f0150be0ca64c2dea66f29eec41797e6101
samlerman <slerman@ur.rochester.edu>
2021-11-13 15:08:37 -0500

    log reg

--- train.py
+++ train.py
@@ -80,9 +80,9 @@ class ConvNet2D(nn.Module):
 #                       nn.Linear(128, 64), nn.ReLU(),
 #                       nn.Linear(64, 7))
 
-# model = ConvNet1D()
+model = ConvNet1D()
 
-model = nn.Sequential(nn.Linear(1800, 7))
+# model = nn.Sequential(nn.Linear(1800, 7))
 
 writer = SummaryWriter(log_dir=args.log_dir)
 
