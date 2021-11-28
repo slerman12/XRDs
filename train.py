0fb79ecf9abdf08e42fec404cf5644412465b62a
samlerman <slerman@ur.rochester.edu>
2021-11-27 19:52:00 -0500

    cnnp 7

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ class ConvNet1DPaper(nn.Module):
             nn.Conv1d(80, 80, kernel_size=50, stride=5),
             nn.AvgPool1d(kernel_size=3, stride=1),
             nn.Conv1d(80, 80, kernel_size=23, stride=2),
-            nn.AvgPool1d(kernel_size=3, stride=1),
+            # nn.AvgPool1d(kernel_size=3, stride=1),
             nn.Flatten(1)
         )
         self.fc = nn.Sequential(
